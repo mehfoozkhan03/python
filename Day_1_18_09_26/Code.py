3455d6b8f07613bdba1d3db0e89547a5d55511db
mehfoozkhan03 <mehfooznettech786@gmail.com>
2026-09-18 17:00:20 +0530

    code change

---
+++ Day_1_18_09_26/Code.py
@@ -0,0 +1,24 @@
+
+# this is 'OUTPUT'
+print('hello world!')
+
+# this is 'VARIABLE' 
+
+name = 'mehooz'
+
+age = 27
+
+# print the value of the variable
+print(type(name))
+
+
+# we have to take input in 'py'
+
+name = input("we have to take number?")
+
+print(name)
+
+# Type Conversion & Type Casting
+
+print(1 + 2.5) # this type conversion (here 'py' will convert auto by interpretor.) # implicit
+print(1 + int(2.999)) # this is type casting (this is totally ignore the float value) # explicit
---
+++ Day_1_18_09_26/Task.py
@@ -0,0 +1,7 @@
+
+
+a = int(input("enter the first_number:"))
+b = int(input("enter the second_number:"))
+
+print(id(a))
+print(id(b))
